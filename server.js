d595b7445153326b5e89f721d322446f017f9014
Daniel Smith <danny@stormpoopersmith.com>
2018-02-02 22:06:31 +0000

    Return roles in sidebar call

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const filehound = require('filehound');
 const path = require('path');
 
@@ -9,29 +8,74 @@ const generalPath = path.join(basePath, 'General');
 const rolesPath = path.join(basePath, 'Roles');
 
 async function sidebar(req, res) {
-  var guides = await filehound.create()
-  .paths(generalPath)
-  .find();
-
   let general = [];
-  guides.forEach(guide => {
-    const parent = path.basename(path.dirname(guide));
-    general.push({
-      name: parent,
-      action: path.parse(guide).name,
-      controller: 'Guides',
-      template: path.join(parent, path.basename(guide)).replace(new RegExp(`\\${path.sep}`, 'g'), '/')
+  await filehound
+    .create()
+    .paths(generalPath)
+    .depth(1)
+    .find()
+    .each(guide => {
+      general.push({
+        name: getParent(guide),
+        action: path.parse(guide).name,
+        controller: 'Guides',
+        template: getTemplate(guide)
+      });
     });
-  });
 
-  var roles = await filehound.create()
+  let roles = [];
+  await filehound
+    .create()
     .paths(rolesPath)
-    .find();
+    .directory()
+    .depth(1)
+    .find()
+    .each(faction => {
+      const factionFolder = path.basename(faction);
+      const factionName = factionFolder.replace(/\d+-/, '');
+      let roleFaction = {
+        name: `The ${factionName}`,
+        action: 'Index',
+        controller: 'Roles',
+        template: `${getTemplate(faction)}.cshtml`,
+        roles: []
+      };
+
+      const factionRoles = filehound
+        .create()
+        .paths(path.join(rolesPath, factionFolder))
+        .discard(`${factionFolder}.cshtml`)
+        .depth(1)
+        .findSync();
+
+      factionRoles.forEach(factionRole => {
+        const roleName = path.parse(factionRole).name;
+        roleFaction.roles.push({
+          name: roleName.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase()).trim(),
+          action: roleName,
+          controller: 'Roles',
+          template: `${factionName}/${getTemplate(factionRole)}`
+        })
+      });
+
+      roles.push(roleFaction);
+    });
 
   return res.json({
-    general
+    general,
+    roles
   });
 }
+
+function getParent(file) {
+  return path.basename(path.dirname(file));
+}
+
+function getTemplate(file) {
+  return path.join(getParent(file), path.basename(file)).replace(new RegExp(`\\${path.sep}`, 'g'), '/')
+}
+
+app.set('json spaces', 2);
 
 app.get('/', sidebar);
 
