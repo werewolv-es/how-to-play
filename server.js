be985fe76c2bdd3318699a2a0937cf04c0e0e8b8
Daniel Smith <danny@stormpoopersmith.com>
2018-02-02 15:23:52 +0000

    Generate JSON for General section

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+const Filehound = require('filehound');
+const path = require('path');
 const express = require('express');
 const app = express();
 
+const basePath = path.join(__dirname, 'guides');
+const generalPath = path.join(basePath, 'General');
+const rolesPath = path.join(basePath, 'Roles');
+
 app.get('/', (req, res) => {
-  const time = Date.now();
-  res.json({"message": time});
+  let response = {};
+
+  Filehound.create()
+    .paths(generalPath)
+    .find().then(files => {
+      let general = [];
+      files.forEach(file => {
+        const parent = path.basename(path.dirname(file));
+        general.push({
+          name: parent,
+          action: path.parse(file).name,
+          controller: 'Guides',
+          tempalte: path.join(parent, path.basename(file))
+        });
+      });
+      response.general = general;
+
+      res.json(response);
+    });
 });
 
 app.listen(8081, ()=>{
