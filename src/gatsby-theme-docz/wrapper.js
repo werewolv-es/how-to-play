4d755d8eaa97ebeb5a37b639bf4d1e8694cf871a
Daniel Smith <danny@stormpoopersmith.com>
2020-04-13 17:24:42 +0100

    Read base URL as part of canonical link, favicon

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useConfig } from 'docz';
 
-const Wrapper = ({ children, doc }) => (
-  <React.Fragment>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <link rel="canonical" href={`/guides${doc.value.route}`} />
-    </Helmet>
-    {children}
-  </React.Fragment>
-);
+const Wrapper = ({ children, doc }) => {
+  const config = useConfig();
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <link rel="canonical" href={`${config.base}${doc.value.route}`} />
+        <link rel="icon" type="image/png" href={`${config.base}favicon.ico`} />
+      </Helmet>
+      {children}
+    </React.Fragment>
+  );
+};
 export default Wrapper;
