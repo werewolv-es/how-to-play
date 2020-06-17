3b7b12e3d741dff988cb63f45be589c761e27e9d
Shane Mc Cormack <shanemcc@gmail.com>
2020-06-17 03:27:04 +0100

    Fix favicon paths. Close #206.

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { withPrefix } from 'gatsby';
 import { useConfig } from 'docz';
 
 const Wrapper = ({ children, doc }) => {
@@ -12,37 +13,37 @@ const Wrapper = ({ children, doc }) => {
 
         <link
           rel="apple-touch-icon"
-          href={`${config.base}apple-touch-icon.png`}
+          href={withPrefix('/apple-touch-icon.png')}
           sizes="180x180"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${config.base}favicon-194x194.png`}
+          href={withPrefix('/favicon-194x194.png')}
           sizes="194x194"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${config.base}android-chrome-192x192.png`}
+          href={withPrefix('/android-chrome-192x192.png')}
           sizes="192x192"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${config.base}favicon-16x16.png`}
+          href={withPrefix('/favicon-16x16.png')}
           sizes="16x16"
         />
-        <link rel="manifest" href={`${config.base}manifest.json`} />
+        <link rel="manifest" href={withPrefix('/manifest.json')} />
         <link
           rel="mask-icon"
-          href={`${config.base}safari-pinned-tab.svg`}
+          href={withPrefix('/safari-pinned-tab.svg')}
           color="#000000"
         />
         <link
           id="favicon"
           rel="shortcut icon"
-          href={`${config.base}favicon.ico`}
+          href={withPrefix('/favicon.ico')}
         />
         <meta name="apple-mobile-web-app-title" content={config.title} />
         <meta name="application-name" content={config.title} />
