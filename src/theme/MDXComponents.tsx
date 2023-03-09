9c9bd91229d5bcae08e0f8ba78529d3239f83d08
Daniel Smith <danny@stormpoopersmith.com>
2023-03-09 20:08:58 +0000

    Install react-trivial-timeline

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the 
 import { library } from "@fortawesome/fontawesome-svg-core"; // Import the library component.
 import { fab } from "@fortawesome/free-brands-svg-icons"; // Import all brands icons.
 import { fas } from "@fortawesome/free-solid-svg-icons"; // Import all solid icons.
+import { Timeline, Event } from "react-trivial-timeline";
+
 
 library.add(fab, fas); // Add all icons to the library so you can use them without importing them individually.
 
@@ -12,4 +14,6 @@ export default {
   // Re-use the default mapping
   ...MDXComponents,
   icon: FontAwesomeIcon, // Make the FontAwesomeIcon component available in MDX as <icon />.
+  timeline: Timeline,
+  event: Event
 };
