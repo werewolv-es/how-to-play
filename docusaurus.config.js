3aedf1d15fc7a71d48119dab6ecd4c57d7f05bec
Daniel Smith <danny@stormpoopersmith.com>
2026-03-24 21:20:41 +0000

    Fix beta deploy and redirect for Docusaurus 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://werewolv.es",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/guides",
+  baseUrl: process.env.SITE_BASEURL ?? "/guides",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "@docusaurus/router";
 
 export default function Home() {
-  return <Redirect to="how-to-play-online-werewolf" />;
+  return <Redirect to="/how-to-play-online-werewolf" />;
 }
